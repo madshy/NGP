4c381cd2a818a243e40fd88c6a2f4bcbe347aa10
madshy_win <madshy94@163.com>
2016-03-20 17:09:10 +0800

    A test case for Database,not complete yet.

---
+++ Server/tests/database.h
@@ -0,0 +1,38 @@
+/***************************************************
+*
+*    @file:test_database.h
+*    @author:madshy
+*    @mail:madshy@163.com
+*    @data:2016/03/20
+*    @comment:A test case for Database
+*
+***************************************************/
+
+#pragma once
+
+#include "../Server/include/Database.h"
+
+void test()
+{
+
+	Database db;
+	if (!db.connect())
+	{
+		return 1;
+	}
+
+	if (!db.insert("insert into ngp(id, name)"
+		"values (8, 'f')"))
+	{
+		qDebug() << "Insert failed.";
+		return 1;
+	}
+
+	QSqlQuery* query = db.select("select * from ngp");
+	while (query->next())
+	{
+		qDebug() << "id:" << query->value(0).toInt();
+		qDebug() << "name:" << query->value("name").toString();
+	}
+
+}
