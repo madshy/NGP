a2fde98facb13a06c7ea19be7592759b179b0f7b
madshy_win <madshy94@163.com>
2016-04-06 15:07:22 +0800

    Add main ui.

---
+++ Client/Client/include/MainUI.h
@@ -0,0 +1,25 @@
+/***************************************************
+*
+*    @file:MainUI.h
+*    @author:madshy
+*    @mail:madshy@163.com
+*    @date:2016/04/06
+*    @comment:MainUI is a user interface after successing to login.
+*
+***************************************************/
+
+#pragma once
+
+#ifndef MAINUI_H
+#define MAINUI_H
+
+#include <qwidget.h>
+
+/*forward declarations*/
+class QLabel;
+class CloseButton;
+class MinimizeButton;
+class QTabWidget;
+
+
+#endif
